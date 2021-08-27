f891d5aa28357929add56b9a1a4950916a0deab3
LuisLobatoCzs <luis.lobatoczs@outlook.com>
2021-08-27 02:23:35 -0500

    Instalación de Font Awesome

--- public/js/app.js
+++ public/js/app.js
@@ -49661,8 +49661,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Luis Lobato\Desktop\miLaravel\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Luis Lobato\Desktop\miLaravel\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Luis Lobato\Desktop\Malgavic\Project\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Luis Lobato\Desktop\Malgavic\Project\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
